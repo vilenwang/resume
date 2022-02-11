4906ed9b3bb8085fc69c445db2bc21003bb8abd3
lukun <911055192@qq.com>
2022-02-12 01:00:24 +0800

    lk

--- webpack-dist.config.js
+++ webpack-dist.config.js
@@ -96,7 +96,7 @@ module.exports = {
     }),
     new EndWebpackPlugin(async () => {
       // 自定义域名
-      fs.writeFileSync(path.resolve(outputPath, 'CNAME'), 'ghresume.heiwawa.xyz');
+      fs.writeFileSync(path.resolve(outputPath, 'CNAME'), 'kunkunj.github.io');
 
       await publishGhPages();
 
